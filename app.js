529a74fae7f0856876ac46adc539d38be5a2b339
Shiv <spsparmar1729@gmail.com>
2021-07-04 09:36:55 +0530

    Added subscription create API

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = express();
 const port = 4000;
 const dotenv = require('dotenv').config();
 const Sequelize = require('sequelize');
+const { Op } = require('sequelize');
+const moment = require('moment');
 
 const User = require('./models').User;
+const Plan = require('./models').Plan;
+const Subscription = require('./models').Subscription;
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -41,6 +45,37 @@ app.put('/user/:user_name', (req, res) => {
     })
     .then(user => res.sendStatus(200))
     .catch(error => res.status(400).send(error));
+});
+
+app.post('/subscription', async (req, res) => {
+  const {user_name, plan_id, start_date} = req.body;
+  // TODO Error handling
+  const plan = await Plan.findOne({where: {plan_id}});
+  const user = await User.findOne({where: {user_name}});
+
+  if (!user || !plan) {
+    return res.status(400).send({status: 'FAILURE', message: "User or Plan not valid"});
+  }
+  // TODO Error handling
+  // Validate date
+  const formattedDate = moment(start_date, 'YYYY-MM-DD', true);
+  if (!formattedDate.isValid()) {
+    return res.status(400).send({status: 'FAILURE', message: "Date not valid"});
+  }
+
+  const validTill = plan.validity > -1 ? formattedDate.clone().add(plan.validity, 'days') : null;
+  return Subscription
+    .create({
+      user_name,
+      plan_id,
+      start_date: formattedDate,
+      valid_till: validTill
+    })
+    .then(subscription => res.status(201).send({
+      status: 'SUCCESS',
+      amount: (-1 * plan.cost).toFixed(2)
+    }))
+    .catch(error => res.status(400).send({status: 'FAILURE', message: error.message}));
 });
 
 app.listen(port, () => {
